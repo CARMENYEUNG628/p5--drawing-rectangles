9b602e52bba57dac981014e8090eb9830cd931e1
Roy Vanegas <roy.vanegas@gmail.com>
2018-10-12 23:02:30 -0400

    Add example of colored fills for each box

--- js/sketch.js
+++ js/sketch.js
@@ -8,11 +8,15 @@ function setup() {
 function draw() {
     noStroke();
 
+    fill(20, 20, 255);
     rect(40, 40, WIDTH, HEIGHT, 10);
 
+    fill(255, 20, 20);
     rect(180, 40, WIDTH, HEIGHT, 10, 20);
 
+    fill(20, 127, 20);
     rect(180, 180, WIDTH, HEIGHT, 10, 20, 30);
 
+    fill(20, 20, 20);
     rect(40, 180, WIDTH, HEIGHT, 10, 20, 30, 40);
 }
