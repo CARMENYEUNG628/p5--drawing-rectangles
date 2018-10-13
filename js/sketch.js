0a5e6e1363a6db558d3d95a0489b98872aa9b80b
Roy Vanegas <roy.vanegas@gmail.com>
2018-10-12 22:57:54 -0400

    Remove colored strokes

--- js/sketch.js
+++ js/sketch.js
@@ -6,15 +6,12 @@ function setup() {
 }
 
 function draw() {
-    stroke(20, 20, 255);
+
     rect(40, 40, WIDTH, HEIGHT, 10);
 
-    stroke(255, 20, 20);
     rect(180, 40, WIDTH, HEIGHT, 10, 20);
 
-    stroke(20, 127, 20);
     rect(180, 180, WIDTH, HEIGHT, 10, 20, 30);
 
-    stroke(20, 20, 20);
     rect(40, 180, WIDTH, HEIGHT, 10, 20, 30, 40);
 }
